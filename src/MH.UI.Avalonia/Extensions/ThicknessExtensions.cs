ffd5a1e257e3c939209250877b317b5038180df0
martin-holy <martin.holy@hotmail.com>
2024-12-25 10:29:52 +0100

    ThicknessExtensions

---
+++ src/MH.UI.Avalonia/Extensions/ThicknessExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using MH.Utils.Types;
+
+namespace MH.UI.Avalonia.Extensions;
+
+public static class ThicknessExtensions {
+  public static Thickness FromThicknessD(this ThicknessD thickness) =>
+    new(thickness.Left, thickness.Top, thickness.Right, thickness.Bottom);
+}
