37cdae09af04a5be83149f6ad63e8d89977e8c9f
martin-holy <martin.holy@hotmail.com>
2024-12-26 10:24:17 +0100

    BaseConverter

---
+++ src/MH.UI.Avalonia/Converters/BaseConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace MH.UI.Avalonia.Converters;
+
+public class BaseConverter : IValueConverter {
+  public virtual object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+    Convert(value, parameter);
+
+  public virtual object? Convert(object? value, object? parameter) =>
+    throw new NotImplementedException();
+
+  public virtual object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+    ConvertBack(value, parameter);
+
+  public virtual object? ConvertBack(object? value, object? parameter) =>
+    throw new NotSupportedException();
+}
