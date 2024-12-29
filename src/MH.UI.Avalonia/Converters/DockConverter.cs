8618bfb1f246b621127212f82f7b7e91a0bba8b7
martin-holy <martin.holy@hotmail.com>
2024-12-29 13:20:42 +0100

    DockConverter

---
+++ src/MH.UI.Avalonia/Converters/DockConverter.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+
+namespace MH.UI.Avalonia.Converters;
+
+public class DockConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static DockConverter? _inst;
+  public static DockConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object? Convert(object? value, object? parameter) =>
+    value == null ? null : (Dock)(int)(MH.UI.Controls.Dock)value;
+
+  public override object? ConvertBack(object? value, object? parameter) =>
+    value == null ? null : (MH.UI.Controls.Dock)(int)(Dock)value;
+}
