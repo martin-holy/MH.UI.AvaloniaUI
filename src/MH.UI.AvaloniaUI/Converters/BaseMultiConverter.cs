8769fb05c2ad1a61748c5755d9463c380fa4ac8d
martin-holy <martin.holy@hotmail.com>
2025-01-01 14:48:49 +0100

    BaseMultiConverter

---
+++ src/MH.UI.AvaloniaUI/Converters/BaseMultiConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace MH.UI.AvaloniaUI.Converters;
+
+public class BaseMultiConverter : IMultiValueConverter {
+  public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture) =>
+    Convert(values, parameter);
+
+  public virtual object Convert(IList<object?> values, object? parameter) =>
+    throw new NotImplementedException();
+}
