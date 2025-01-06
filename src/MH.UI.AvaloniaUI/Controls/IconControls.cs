dcb7406c40719470135e021ddcdffe0c978cd126
martin-holy <martin.holy@hotmail.com>
2025-01-06 10:14:40 +0100

    IconTextBlock Compact mode

--- src/MH.UI.AvaloniaUI/Controls/IconControls.cs
+++ src/MH.UI.AvaloniaUI/Controls/IconControls.cs
@@ -8,6 +8,8 @@ namespace MH.UI.AvaloniaUI.Controls;
 public enum IconTextBlockShadow { None, Icon, Text, Both }
 
 public class IconTextBlock : ContentControl {
+  private Grid? _partGrid;
+
   public static readonly StyledProperty<bool> CompactProperty = AvaloniaProperty.Register<IconTextBlock, bool>(nameof(Compact));
   public static readonly StyledProperty<IStyle> TextBorderStyleProperty = AvaloniaProperty.Register<IconTextBlock, IStyle>(nameof(TextBorderStyle));
   public static readonly StyledProperty<IconTextBlockShadow> ShadowProperty = AvaloniaProperty.Register<IconTextBlock, IconTextBlockShadow>(nameof(Shadow));
@@ -15,8 +17,34 @@ public class IconTextBlock : ContentControl {
   public bool Compact { get => GetValue(CompactProperty); set => SetValue(CompactProperty, value); }
   public IStyle TextBorderStyle { get => GetValue(TextBorderStyleProperty); set => SetValue(TextBorderStyleProperty, value); }
   public IconTextBlockShadow Shadow { get => GetValue(ShadowProperty); set => SetValue(ShadowProperty, value); }
-}
 
+  static IconTextBlock() {
+    CompactProperty.Changed.AddClassHandler<IconTextBlock>(_onCompactChanged);
+  }
+
+  protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+    base.OnApplyTemplate(e);
+    _partGrid = e.NameScope.Find<Grid>("PART_Grid");
+    SetCompactGrid();
+  }
+
+  private static void _onCompactChanged(IconTextBlock o, AvaloniaPropertyChangedEventArgs e) =>
+    o.SetCompactGrid();
+
+  internal void SetCompactGrid() {
+    if (_partGrid == null) return;
+
+    _partGrid.ColumnDefinitions.Clear();
+    if (Compact) {
+      _partGrid.ColumnDefinitions.Add(new(GridLength.Star));
+      _partGrid.ColumnDefinitions.Add(new(GridLength.Auto));
+    }
+    else {
+      _partGrid.ColumnDefinitions.Add(new(GridLength.Auto));
+      _partGrid.ColumnDefinitions.Add(new(GridLength.Star));
+    }
+  }
+}
 
 public class IconButton : Button;
 public class IconTextButton : Button;
