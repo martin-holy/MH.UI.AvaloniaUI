19e5bcc601d8a54d39b7513e0a35c2ff37ea2ffe
martin-holy <martin.holy@hotmail.com>
2025-01-11 12:30:02 +0100

    DockExtensions

---
+++ src/MH.UI.AvaloniaUI/Extensions/DockExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+
+namespace MH.UI.AvaloniaUI.Extensions;
+
+public static class DockExtensions {
+  public static Dock? FromMhDock(this MH.UI.Controls.Dock dock) =>
+    dock switch {
+      UI.Controls.Dock.Left => Dock.Left,
+      UI.Controls.Dock.Top => Dock.Top,
+      UI.Controls.Dock.Right => Dock.Right,
+      UI.Controls.Dock.Bottom => Dock.Bottom,
+      _ => null
+    };
+
+  public static UI.Controls.Dock? ToMhDock(this Dock dock) =>
+    dock switch {
+      Dock.Left => UI.Controls.Dock.Left,
+      Dock.Bottom => UI.Controls.Dock.Bottom,
+      Dock.Right => UI.Controls.Dock.Right,
+      Dock.Top => UI.Controls.Dock.Top,
+      _ => null
+    };
+}
