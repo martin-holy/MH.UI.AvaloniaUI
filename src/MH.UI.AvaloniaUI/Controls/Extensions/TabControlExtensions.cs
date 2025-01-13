1d968b32e788c628e93cc9e20606ec7f14ec64ff
martin-holy <martin.holy@hotmail.com>
2025-01-13 13:49:41 +0100

    TabControlExtensions: UpdateMaxTabSize

---
+++ src/MH.UI.AvaloniaUI/Controls/Extensions/TabControlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace MH.UI.AvaloniaUI.Controls.Extensions;
+
+public static class TabControlExtensions {
+  static TabControlExtensions() {
+    Visual.BoundsProperty.Changed.AddClassHandler<TabControl>(_onBoundsChanged);
+  }
+
+  private static void _onBoundsChanged(TabControl o, AvaloniaPropertyChangedEventArgs e) {
+    if (e.Sender is not TabControl { DataContext: UI.Controls.TabControl tc } || e.NewValue is not Rect rect) return;
+    tc.UpdateMaxTabSize(rect.Width, rect.Height);
+  }
+
+  public static void Init() { }
+}
