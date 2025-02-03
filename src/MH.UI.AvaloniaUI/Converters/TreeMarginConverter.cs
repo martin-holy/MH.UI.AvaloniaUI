4b393d5b8c82ade22d873648d748d658c808ebf8
martin-holy <martin.holy@hotmail.com>
2025-02-03 11:19:28 +0100

    TreeMarginConverter

---
+++ src/MH.UI.AvaloniaUI/Converters/TreeMarginConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using MH.Utils;
+using MH.Utils.Interfaces;
+
+namespace MH.UI.AvaloniaUI.Converters;
+
+public class TreeMarginConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static TreeMarginConverter? _inst;
+  public static TreeMarginConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object Convert(object? value, object? parameter) {
+    var level = value is ITreeItem ti ? ti.GetLevel() : 0;
+    var length = int.TryParse(parameter as string, out var l) ? l : 0;
+
+    return new Thickness(length * level, 0.0, 0.0, 0.0);
+  }
+}
