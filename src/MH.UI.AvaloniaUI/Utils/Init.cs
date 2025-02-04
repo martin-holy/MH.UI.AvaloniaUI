d45098c83ba56cba4681d220782545a45b16ad0b
martin-holy <martin.holy@hotmail.com>
2025-02-04 10:12:05 +0100

    Keyboard Modifiers

--- src/MH.UI.AvaloniaUI/Utils/Init.cs
+++ src/MH.UI.AvaloniaUI/Utils/Init.cs
@@ -10,11 +10,12 @@ public static class Init {
 
   // TODO PORT
   public static void SetDelegates() {
-    /*MH.Utils.Keyboard.IsCtrlOn = () => (Keyboard.Modifiers & ModifierKeys.Control) > 0;
-    MH.Utils.Keyboard.IsAltOn = () => (Keyboard.Modifiers & ModifierKeys.Alt) > 0;
-    MH.Utils.Keyboard.IsShiftOn = () => (Keyboard.Modifiers & ModifierKeys.Shift) > 0;
+    KeyboardHelper.Init();
+    MH.Utils.Keyboard.IsCtrlOn = () => KeyboardHelper.IsCtrlOn;
+    MH.Utils.Keyboard.IsAltOn = () => KeyboardHelper.IsAltOn;
+    MH.Utils.Keyboard.IsShiftOn = () => KeyboardHelper.IsShiftOn;
 
-    MH.Utils.Clipboard.SetText = Clipboard.SetText;
+    /*MH.Utils.Clipboard.SetText = Clipboard.SetText;
 
     MH.Utils.Imaging.GetBitmapHashPixels = Imaging.GetBitmapHashPixels;
     MH.Utils.Imaging.ResizeJpg = Imaging.ResizeJpg;
