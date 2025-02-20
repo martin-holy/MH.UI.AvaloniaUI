7f43da3cbe134c8375816b45e04a1a84db3d1b2d
martin-holy <martin.holy@hotmail.com>
2025-02-20 14:34:46 +0100

    CollectionViewItemTemplateConverter

---
+++ src/MH.UI.AvaloniaUI/Converters/CollectionViewItemTemplateConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Templates;
+using Avalonia.VisualTree;
+using MH.UI.Interfaces;
+using MH.Utils.Interfaces;
+
+namespace MH.UI.AvaloniaUI.Converters;
+
+public sealed class CollectionViewItemTemplateConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static CollectionViewItemTemplateConverter? _inst;
+  public static CollectionViewItemTemplateConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object? Convert(object? value, object? parameter) =>
+    value is Visual v && v.FindAncestorOfType<StackPanel>() is
+      { DataContext: ITreeItem { Parent: ICollectionViewGroup g } }
+      ? ResourceConverter.Inst.Convert(g.GetItemTemplateName(), null) as DataTemplate
+      : null;
+}
