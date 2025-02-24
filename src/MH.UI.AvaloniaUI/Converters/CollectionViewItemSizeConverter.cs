94b7b60470a2b2d4b651c54e8634fd7b617a2cc9
martin-holy <martin.holy@hotmail.com>
2025-02-24 09:11:21 +0100

    CollectionViewItemSizeConverter

---
+++ src/MH.UI.AvaloniaUI/Converters/CollectionViewItemSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using MH.UI.Interfaces;
+using MH.Utils.Interfaces;
+
+namespace MH.UI.AvaloniaUI.Converters;
+
+public class CollectionViewItemSizeConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static CollectionViewItemSizeConverter? _inst;
+  public static CollectionViewItemSizeConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object? Convert(object? value, object? parameter) {
+    if (value is Control { Parent.DataContext: ITreeItem { Parent: ICollectionViewGroup g } } c) {
+      c.Width = g.GetItemSize(c.DataContext!, true);
+      c.Height = g.GetItemSize(c.DataContext!, false);
+    }
+    
+    return null;
+  }
+}
