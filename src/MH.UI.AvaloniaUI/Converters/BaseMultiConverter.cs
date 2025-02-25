1281472f6430c1c5bc10f3940d2e95bfd17654ce
martin-holy <martin.holy@hotmail.com>
2025-02-25 08:15:34 +0100

    BaseMultiConverter: nullable

--- src/MH.UI.AvaloniaUI/Converters/BaseMultiConverter.cs
+++ src/MH.UI.AvaloniaUI/Converters/BaseMultiConverter.cs
@@ -7,6 +7,6 @@ public class BaseMultiConverter : IMultiValueConverter {
   public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture) =>
     Convert(values, parameter);
 
-  public virtual object Convert(IList<object?> values, object? parameter) =>
+  public virtual object? Convert(IList<object?> values, object? parameter) =>
     throw new NotImplementedException();
 }
