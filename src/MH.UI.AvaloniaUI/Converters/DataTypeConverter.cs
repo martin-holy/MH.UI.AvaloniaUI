988970372f96df952b8a3a7ec25bdecdf9179bfe
martin-holy <martin.holy@hotmail.com>
2025-03-03 14:11:41 +0100

    DataTypeConverter

---
+++ src/MH.UI.AvaloniaUI/Converters/DataTypeConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace MH.UI.AvaloniaUI.Converters;
+
+public class DataTypeConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static DataTypeConverter? _inst;
+  public static DataTypeConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object? Convert(object? value, object? parameter) =>
+    value != null && parameter is Type pType && (pType.IsInterface
+      ? value.GetType().GetInterface(pType.Name) != null
+      : value.GetType().IsAssignableTo(pType));
+}
