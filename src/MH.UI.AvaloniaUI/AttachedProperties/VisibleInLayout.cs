c9532d51f18e872953ccbbff5884de9669878300
martin-holy <martin.holy@hotmail.com>
2025-03-04 14:06:25 +0100

    VisibleInLayout.When attached property

---
+++ src/MH.UI.AvaloniaUI/AttachedProperties/VisibleInLayout.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+
+namespace MH.UI.AvaloniaUI.AttachedProperties;
+
+public class VisibleInLayout {
+  public static readonly AttachedProperty<bool> WhenProperty =
+    AvaloniaProperty.RegisterAttached<VisibleInLayout, Control, bool>(
+    "When", false, false, BindingMode.OneWay, null, _onWhenCoerced);
+
+  public static bool GetWhen(Control control) => control.GetValue(WhenProperty);
+  public static void SetWhen(Control control, bool value) => control.SetValue(WhenProperty, value);
+
+  private static bool _onWhenCoerced(AvaloniaObject control, bool value) {
+    if (control is Control c) {
+      c.IsHitTestVisible = value;
+      c.Opacity = value ? 1 : 0;
+    }
+
+    return value;
+  }
+}
