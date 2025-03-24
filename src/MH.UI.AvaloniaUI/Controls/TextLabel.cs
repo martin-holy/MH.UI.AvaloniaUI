dc88a1c67571b73713958f58655f6851e9a1cc40
martin-holy <martin.holy@hotmail.com>
2025-03-24 09:43:23 +0100

    TextLabel

---
+++ src/MH.UI.AvaloniaUI/Controls/TextLabel.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace MH.UI.AvaloniaUI.Controls;
+
+public class TextLabel : ContentControl {
+  public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<TextLabel, string?>(nameof(Text));
+  public string? Text { get => GetValue(TextProperty); set => SetValue(TextProperty, value); }
+}
